a9acf96c3acd90bdd4f61c3523ec9b17c5abdad8
Iman Khoshabi <iman.neofight@gmail.com>
2024-07-09 21:41:50 +0200

    Add sprites and parallax background

--- lib/flappy_dash_game.dart
+++ lib/flappy_dash_game.dart
@@ -1,9 +1,8 @@
-import 'dart:math';
-import 'dart:ui';
-
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
-import 'package:flame/palette.dart';
+
+import 'component/dash.dart';
+import 'component/dash_parallax_background.dart';
 
 class FlappyDashGame extends FlameGame<FlappyDashWorld> {
   FlappyDashGame()
@@ -20,40 +19,7 @@ class FlappyDashWorld extends World {
   @override
   void onLoad() {
     super.onLoad();
+    add(DashParallaxBackground());
     add(Dash());
-    add(RectangleComponent(
-      position: Vector2(10.0, 15.0),
-      size: Vector2.all(10),
-      angle: pi / 2,
-      anchor: Anchor.center,
-    ));
-  }
-}
-
-class Dash extends PositionComponent {
-  Dash()
-      : super(
-          position: Vector2(0, 0),
-          size: Vector2(40, 40),
-        );
-
-  @override
-  void update(double dt) {
-    super.update(dt);
-    position = Vector2.zero();
-    angle += 0.01;
-  }
-
-  @override
-  void render(Canvas canvas) {
-    super.render(canvas);
-    canvas.drawRect(
-      Rect.fromCenter(
-        center: Offset.zero,
-        width: size.x,
-        height: size.y,
-      ),
-      BasicPalette.red.paint(),
-    );
   }
 }
