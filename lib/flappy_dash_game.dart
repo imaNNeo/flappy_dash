6c37d42678c7f8c3e811f33b8106f27fe5ee2b41
Iman Khoshabi <iman.neofight@gmail.com>
2024-07-11 23:14:53 +0200

    Player jump movement

--- lib/flappy_dash_game.dart
+++ lib/flappy_dash_game.dart
@@ -1,10 +1,13 @@
 import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'component/dash.dart';
 import 'component/dash_parallax_background.dart';
 
-class FlappyDashGame extends FlameGame<FlappyDashWorld> {
+class FlappyDashGame extends FlameGame<FlappyDashWorld> with KeyboardEvents {
   FlappyDashGame()
       : super(
           world: FlappyDashWorld(),
@@ -13,13 +16,42 @@ class FlappyDashGame extends FlameGame<FlappyDashWorld> {
             height: 1000,
           ),
         );
+
+  @override
+  KeyEventResult onKeyEvent(
+      KeyEvent event,
+      Set<LogicalKeyboardKey> keysPressed,
+      ) {
+    final isKeyDown = event is KeyDownEvent;
+
+    final isSpace = keysPressed.contains(LogicalKeyboardKey.space);
+
+    if (isSpace && isKeyDown) {
+      world.onSpaceDown();
+      return KeyEventResult.handled;
+    }
+    return KeyEventResult.ignored;
+  }
 }
 
-class FlappyDashWorld extends World {
+class FlappyDashWorld extends World with TapCallbacks {
+
+  late Dash _dash;
+
   @override
   void onLoad() {
     super.onLoad();
     add(DashParallaxBackground());
-    add(Dash());
+    add(_dash = Dash());
+  }
+
+  @override
+  void onTapDown(TapDownEvent event) {
+    super.onTapDown(event);
+    _dash.jump();
+  }
+
+  void onSpaceDown() {
+    _dash.jump();
   }
 }
