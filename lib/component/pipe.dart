16ee209f73297fc8e8ec77443b5e47d7e6eaf1e5
Iman Khoshabi <iman.neofight@gmail.com>
2024-07-17 00:39:11 +0200

    Add moving pipes

---
+++ lib/component/pipe.dart
@@ -0,0 +1,36 @@
+import 'package:flame/components.dart';
+import 'package:flame/extensions.dart';
+
+class Pipe extends PositionComponent {
+  late Sprite _pipeSprite;
+
+  final bool isFlipped;
+
+  Pipe({
+    required this.isFlipped,
+    required super.position,
+  }): super(priority: 2);
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+    _pipeSprite = await Sprite.load('pipe.png');
+    anchor = Anchor.topCenter;
+    final ratio = _pipeSprite.srcSize.y / _pipeSprite.srcSize.x;
+    const width = 82.0;
+    size = Vector2(width, width * ratio);
+    if (isFlipped) {
+      flipVertically();
+    }
+  }
+
+  @override
+  void render(Canvas canvas) {
+    super.render(canvas);
+    _pipeSprite.render(
+      canvas,
+      position: Vector2.zero(),
+      size: size,
+    );
+  }
+}
--- lib/flappy_dash_game.dart
+++ lib/flappy_dash_game.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
@@ -6,6 +8,8 @@ import 'package:flutter/services.dart';
 
 import 'component/dash.dart';
 import 'component/dash_parallax_background.dart';
+import 'component/pipe.dart';
+import 'component/pipe_pair.dart';
 
 class FlappyDashGame extends FlameGame<FlappyDashWorld> with KeyboardEvents {
   FlappyDashGame()
@@ -19,9 +23,9 @@ class FlappyDashGame extends FlameGame<FlappyDashWorld> with KeyboardEvents {
 
   @override
   KeyEventResult onKeyEvent(
-      KeyEvent event,
-      Set<LogicalKeyboardKey> keysPressed,
-      ) {
+    KeyEvent event,
+    Set<LogicalKeyboardKey> keysPressed,
+  ) {
     final isKeyDown = event is KeyDownEvent;
 
     final isSpace = keysPressed.contains(LogicalKeyboardKey.space);
@@ -34,15 +38,40 @@ class FlappyDashGame extends FlameGame<FlappyDashWorld> with KeyboardEvents {
   }
 }
 
-class FlappyDashWorld extends World with TapCallbacks {
-
+class FlappyDashWorld extends World with TapCallbacks, HasGameRef<FlappyDashGame> {
   late Dash _dash;
+  late PipePair _lastPipe;
+  static const _pipesDistance = 400.0;
 
   @override
   void onLoad() {
     super.onLoad();
     add(DashParallaxBackground());
     add(_dash = Dash());
+    _generatePipes(
+      fromX: 350,
+    );
+  }
+
+  void _generatePipes({
+    int count = 5,
+    double fromX = 0.0,
+  }) {
+    for (int i = 0; i < count; i++) {
+      const area = 600;
+      final y = (Random().nextDouble() * area) - (area / 2);
+      add(_lastPipe = PipePair(
+        position: Vector2(fromX + (i * _pipesDistance), y),
+      ));
+    }
+  }
+
+  void _removePipes() {
+    final pipes = children.whereType<PipePair>();
+    final shouldBeRemoved = max(pipes.length - 5, 0);
+    pipes.take(shouldBeRemoved).forEach((pipe) {
+      pipe.removeFromParent();
+    });
   }
 
   @override
@@ -53,5 +82,17 @@ class FlappyDashWorld extends World with TapCallbacks {
 
   void onSpaceDown() {
     _dash.jump();
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    if (_dash.x >= _lastPipe.x) {
+      _generatePipes(
+        fromX: _pipesDistance,
+      );
+      _removePipes();
+    }
+    game.camera.viewfinder.zoom = 1.0;
   }
 }
