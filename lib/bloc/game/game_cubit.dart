a87f68b8223f004d4f8e0e6e03d91b602ecc4401
Iman Khoshabi <iman.neofight@gmail.com>
2024-09-08 21:34:08 +0200

    Add some temp code related to the nakama

--- lib/bloc/game/game_cubit.dart
+++ lib/bloc/game/game_cubit.dart
@@ -1,15 +1,41 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flappy_dash/audio_helper.dart';
+import 'package:nakama/nakama.dart';
 
 part 'game_state.dart';
 
 class GameCubit extends Cubit<GameState> {
   GameCubit(
     this._audioHelper,
-  ) : super(const GameState());
+  ) : super(const GameState()) {
+    _initializeNakama();
+  }
 
   final AudioHelper _audioHelper;
+
+  void _initializeNakama() async {
+    final client = getNakamaClient(
+      host: '164.90.207.244',
+      ssl: false,
+      serverKey: 'defaultkey',
+      grpcPort: 7349,
+      // optional
+      httpPort: 7350, // optional
+    );
+    final session = await client.authenticateDevice(
+      deviceId: 'test-device-id',
+      username: 'iman_neo',
+    );
+    print('Session is: ${session.token}');
+
+    final group = await client.createGroup(
+      session: session,
+      name: 'Flutter devs',
+      description: 'This is a cool group for Flutter devs!'
+    );
+    print('Group is created: ${group.id}');
+  }
 
   void startPlaying() {
     _audioHelper.playBackgroundAudio();
