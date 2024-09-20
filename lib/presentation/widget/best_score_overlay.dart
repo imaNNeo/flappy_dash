201d8f1db39a205cb089132a11e4456bb5e7caf0
Iman Khoshabi <iman.neofight@gmail.com>
2024-09-20 23:31:59 +0200

    Move InkWell inside the box_overlay (this way we can re-use it)

--- lib/presentation/widget/best_score_overlay.dart
+++ lib/presentation/widget/best_score_overlay.dart
@@ -14,67 +14,59 @@ class BestScoreOverlay extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: Colors.transparent,
-      child: InkWell(
-        borderRadius: const BorderRadius.all(
-          Radius.circular(16),
-        ),
-        onTap: onTap,
-        child: BoxOverlay(
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
+    return BoxOverlay(
+      onTap: onTap,
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          SizedBox(
+            width: 32,
+            height: 32,
+            child: Stack(
+              children: [
+                SvgPicture.asset(
+                  'assets/icons/ic_trophy.svg',
+                  height: 32,
+                  colorFilter: const ColorFilter.mode(
+                    AppColors.leaderboardGoldenColor,
+                    BlendMode.srcIn,
+                  ),
+                ),
+                const Align(
+                  alignment: Alignment(0.0, -0.8),
+                  child: Text(
+                    '2',
+                    style: TextStyle(
+                      color: AppColors.leaderboardGoldenColorText,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(width: 18),
+          const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(
-                width: 32,
-                height: 32,
-                child: Stack(
-                  children: [
-                    SvgPicture.asset(
-                      'assets/icons/ic_trophy.svg',
-                      height: 32,
-                      colorFilter: const ColorFilter.mode(
-                        AppColors.leaderboardGoldenColor,
-                        BlendMode.srcIn,
-                      ),
-                    ),
-                    const Align(
-                      alignment: Alignment(0.0, -0.8),
-                      child: Text(
-                        '2',
-                        style: TextStyle(
-                          color: AppColors.leaderboardGoldenColorText,
-                          fontSize: 18,
-                        ),
-                      ),
-                    ),
-                  ],
+              Text(
+                'Your best',
+                style: TextStyle(
+                  color: AppColors.whiteTextColor,
+                  fontSize: 12,
+                  fontFamily: 'Roboto',
                 ),
               ),
-              const SizedBox(width: 18),
-              const Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Your best',
-                    style: TextStyle(
-                      color: AppColors.whiteTextColor,
-                      fontSize: 12,
-                      fontFamily: 'Roboto',
-                    ),
-                  ),
-                  Text(
-                    '122',
-                    style: TextStyle(
-                      color: AppColors.mainColor,
-                      fontSize: 22,
-                    ),
-                  ),
-                ],
+              Text(
+                '122',
+                style: TextStyle(
+                  color: AppColors.mainColor,
+                  fontSize: 22,
+                ),
               ),
             ],
           ),
-        ),
+        ],
       ),
     );
   }
--- lib/presentation/widget/box_overlay.dart
+++ lib/presentation/widget/box_overlay.dart
@@ -5,24 +5,35 @@ class BoxOverlay extends StatelessWidget {
   const BoxOverlay({
     super.key,
     required this.child,
+    this.onTap,
   });
 
   final Widget child;
+  final VoidCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        color: AppColors.boxBgColor,
-        borderRadius: BorderRadius.all(
-          Radius.circular(16.0),
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        borderRadius: const BorderRadius.all(
+          Radius.circular(16),
+        ),
+        onTap: onTap,
+        child: Container(
+          decoration: const BoxDecoration(
+            color: AppColors.boxBgColor,
+            borderRadius: BorderRadius.all(
+              Radius.circular(16.0),
+            ),
+          ),
+          padding: const EdgeInsets.symmetric(
+            horizontal: 18.0,
+            vertical: 12.0,
+          ),
+          child: child,
         ),
       ),
-      padding: const EdgeInsets.symmetric(
-        horizontal: 18.0,
-        vertical: 12.0,
-      ),
-      child: child,
     );
   }
 }
