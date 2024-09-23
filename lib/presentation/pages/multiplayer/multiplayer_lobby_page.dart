9098728040d9f24def8436844e28257e666023e5
Iman Khoshabi <iman.neofight@gmail.com>
2024-09-23 22:55:11 +0200

    Implement basic UI of multiplayer lobby page

--- lib/presentation/pages/multiplayer/multiplayer_lobby_page.dart
+++ lib/presentation/pages/multiplayer/multiplayer_lobby_page.dart
@@ -1,24 +1,46 @@
+import 'package:flappy_dash/presentation/responsive/screen_size.dart';
+import 'package:flappy_dash/presentation/widget/blurred_background.dart';
+import 'package:flappy_dash/presentation/widget/game_title.dart';
+import 'package:flappy_dash/presentation/widget/github_button.dart';
+import 'package:flappy_dash/presentation/widget/watch_on_youtube_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 
 class MultiPlayerLobbyPage extends StatelessWidget {
   const MultiPlayerLobbyPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final screenSize = ScreenSize.fromContext(context);
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            const Text('Multiplayer Lobby Page'),
-            ElevatedButton(
-              onPressed: () {
-                context.replace('/multi_player:asdfasdf');
-              },
-              child: const Text('Multi Player'),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          const BlurredBackground(),
+          SafeArea(
+            child: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 18),
+                  GameTitle(
+                    screenSize: screenSize,
+                    showMultiplayerText: true,
+                  ),
+                  Expanded(child: Container()),
+                  WatchOnYoutubeWidget(screenSize: screenSize),
+                  const SizedBox(height: 16),
+                ],
+              ),
             ),
-          ],
-        ),
+          ),
+          Align(
+            alignment: Alignment.topRight,
+            child: GithubButton(
+              screenSize: screenSize,
+            ),
+          )
+        ],
       ),
     );
   }
