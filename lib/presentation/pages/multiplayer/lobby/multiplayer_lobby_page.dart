714f00a3aa35efbf305344f3c522ec6bb0756325
Iman Khoshabi <iman.neofight@gmail.com>
2024-09-24 00:09:19 +0200

    Implement the multiplayer lobby page (with responsiveness)

--- lib/presentation/pages/multiplayer/lobby/multiplayer_lobby_page.dart
+++ lib/presentation/pages/multiplayer/lobby/multiplayer_lobby_page.dart
@@ -1,9 +1,15 @@
+import 'package:flappy_dash/presentation/app_style.dart';
 import 'package:flappy_dash/presentation/responsive/screen_size.dart';
+import 'package:flappy_dash/presentation/widget/big_button.dart';
 import 'package:flappy_dash/presentation/widget/blurred_background.dart';
+import 'package:flappy_dash/presentation/widget/dash_player_box.dart';
 import 'package:flappy_dash/presentation/widget/game_title.dart';
 import 'package:flappy_dash/presentation/widget/github_button.dart';
+import 'package:flappy_dash/presentation/widget/transparent_content_box.dart';
 import 'package:flappy_dash/presentation/widget/watch_on_youtube_widget.dart';
 import 'package:flutter/material.dart';
+
+part 'parts/pending_match_box.dart';
 
 class MultiPlayerLobbyPage extends StatelessWidget {
   const MultiPlayerLobbyPage({super.key});
@@ -11,6 +17,18 @@ class MultiPlayerLobbyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final screenSize = ScreenSize.fromContext(context);
+    final boxVerticalSpacing = switch (screenSize) {
+      ScreenSize.extraSmall => 18.0,
+      ScreenSize.small => 28.0,
+      ScreenSize.medium => 38.0,
+      ScreenSize.large || ScreenSize.extraLarge => 48.0,
+    };
+    final boxHorizontalPadding = switch (screenSize) {
+      ScreenSize.extraSmall => 10.0,
+      ScreenSize.small => 12.0,
+      ScreenSize.medium => 22.0,
+      ScreenSize.large || ScreenSize.extraLarge => 28.0,
+    };
     return Scaffold(
       body: Stack(
         fit: StackFit.expand,
@@ -27,7 +45,13 @@ class MultiPlayerLobbyPage extends StatelessWidget {
                     screenSize: screenSize,
                     showMultiplayerText: true,
                   ),
-                  Expanded(child: Container()),
+                  SizedBox(height: boxVerticalSpacing * 1.5),
+                  Expanded(
+                    child: PendingMatchBox(
+                      horizontalPadding: boxHorizontalPadding,
+                    ),
+                  ),
+                  SizedBox(height: boxVerticalSpacing),
                   WatchOnYoutubeWidget(screenSize: screenSize),
                   const SizedBox(height: 16),
                 ],
