e41742d58b46afbbc5c370eed866f0176a499cec
Iman Khoshabi <iman.neofight@gmail.com>
2024-11-12 20:37:15 +0100

    Add new debug usernames

--- lib/domain/app_constants.dart
+++ lib/domain/app_constants.dart
@@ -1,7 +1,9 @@
 class AppConstants {
   static const String gitHubUrl = 'https://github.com/imaNNeo/flappy_dash';
-  static const String youTubeUrl = 'https://www.youtube.com/playlist?list=PL1-_rCwRcnbMVXe7jyCvZixrMRbvLTXG3';
+  static const String youTubeUrl =
+      'https://www.youtube.com/playlist?list=PL1-_rCwRcnbMVXe7jyCvZixrMRbvLTXG3';
   static const debugUsernames = [
     'DXPKRLOnkl',
+    'cuAwnDbTcj',
   ];
 }
